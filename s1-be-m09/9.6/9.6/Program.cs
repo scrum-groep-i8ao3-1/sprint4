68c386b5d4e13c6a0d665fdfd7476ff82d62219f
dannyoo7 <dannylanssink@outloo.com>
2019-02-25 10:54:01 +0100

    9.6

---
+++ s1-be-m09/9.6/9.6/Program.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean jarig = false;
+            string path = @"E:\scrum\sprint4\s1-be-m09\9.6\9.6\namen.txt.txt";
+            string[,] ned = new string[4, 2]; //namen en datums [ned]
+            StreamReader sr = new StreamReader(path);
+            var lineCount = File.ReadLines(path).Count(); // het nummer hoeveel lines er heeft [ wil je er 1 toevoegen]
+            for (int i = 0;i < lineCount; i++)
+            {
+                string lijn =sr.ReadLine();
+                string[] array = lijn.Split(' ');
+                string naam = array[0];
+                string datum = array[1];
+                string today = DateTime.Today.ToString("dd/MM");
+                if (datum == today)
+                {
+                    Console.WriteLine(naam + "is jarig");
+                    jarig = true;
+                }
+
+            }
+            if(jarig == true)
+            {
+                string locatie = @"E:\scrum\sprint4\s1-be-m09\9.6\9.6\Happy_Birthday_to_You-osKLrBxqkuA.wav";
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(locatie);
+                player.Play();
+            }
+            else
+            {
+                Console.WriteLine("er is niemand jarig ;{");
+            }
+            Console.ReadLine();
+        }
+    }
+}
