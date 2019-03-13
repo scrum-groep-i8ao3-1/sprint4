acd1da4509ad9a39c67f131639ce602f6d042d80
yaboySLAM <44971734+yaboySLAM@users.noreply.github.com>
2019-03-13 11:15:12 +0100

    gey

---
+++ s1-be-m09/ConsoleApp1/test 2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+
+namespace LeftRightMid
+{
+    /// 
+    /// Summary description for Class1.
+    /// 
+    class LeftRightMid
+    {
+        /// 
+        /// The main entry point for the application.
+        /// 
+        [STAThread]
+        static void Main(string[] args)
+        {
+
+            //assign a value to our string
+            string myString = "This is a string";
+            //get 4 characters starting from the left
+            Console.WriteLine(Left(myString, 4));
+            //get 6 characters starting from the right
+            Console.WriteLine(Right(myString, 6));
+            //get 4 characters starting at index 5 of the string
+            Console.WriteLine(Mid(myString, 5, 4));
+            //get the characters from index 5 up to the end of the string
+            Console.WriteLine(Mid(myString, 5));
+            //display the result to the screen
+            Console.ReadLine();
+        }
+
+        public static string Left(string param, int length)
+        {
+            //we start at 0 since we want to get the characters starting from the
+            //left and with the specified lenght and assign it to a variable
+            string result = param.Substring(0, length);
+            //return the result of the operation
+            return result;
+        }
+        public static string Right(string param, int length)
+        {
+            //start at the index based on the lenght of the sting minus
+            //the specified lenght and assign it a variable
+            string result = param.Substring(param.Length - length, length);
+            //return the result of the operation
+            return result;
+        }
+
+        public static string Mid(string param, int startIndex, int length)
+        {
+            //start at the specified index in the string ang get N number of
+            //characters depending on the lenght and assign it to a variable
+            string result = param.Substring(startIndex, length);
+            //return the result of the operation
+            return result;
+        }
+
+        public static string Mid(string param, int startIndex)
+        {
+            //start at the specified index and return all characters after it
+            //and assign it to a variable
+            string result = param.Substring(startIndex);
+            //return the result of the operation
+            return result;
+        }
+
+    }
+}
---
+++ s1-be-m09/ConsoleApp1/test/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Timers;
+
+public class Example
+{
+    public static int maximumteller = 0;
+    private static System.Timers.Timer aTimer;
+    public static string teller = string.Empty;
+    public static string userK = "";
+    public static double procent = 0;
+    public static int maximumgetal = 0;
+    public static void Main()
+    {
+
+        Console.WriteLine("vul in welk karakter u wult gebruiken: ");
+        userK = Convert.ToString(Console.ReadLine());
+        Console.WriteLine("hoeveel van deze karakters wild u hebben: ");
+        maximumgetal = Convert.ToInt32(Console.ReadLine());
+        SetTimer();
+        Console.WriteLine(" ");
+        Console.ReadLine();
+        aTimer.Stop();
+        aTimer.Dispose();
+
+    }
+
+    private static void SetTimer()
+    {
+        // Create a timer with a two second interval.
+        aTimer = new System.Timers.Timer(500);
+        // Hook up the Elapsed event for the timer.
+        aTimer.Elapsed += OnTimedEvent;
+        aTimer.AutoReset = true;
+        aTimer.Enabled = true;
+    }
+
+
+    private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+    {
+        string tellers = "";
+        procent += ((maximumteller / maximumgetal) * 100);
+        if (maximumteller < maximumgetal)
+        {
+            Console.Clear();
+            teller += userK;
+            tellers = teller.ToString();
+            Console.WriteLine("Karakter: " + userK);
+            Console.WriteLine("Hoeveelheid Karakters: " + (maximumgetal));
+            Console.Write(procent + "% ");
+            Console.WriteLine(tellers);
+            tellers += " ";
+            maximumteller += 1;
+        }
+        else
+        {
+            Console.Clear();
+            teller += userK;
+            tellers = teller.ToString();
+            Console.WriteLine("Karakter: " + userK);
+            Console.WriteLine("Hoeveelheid Karakters: " + (maximumgetal));
+            Console.Write(procent + "% ");
+            Console.WriteLine(tellers);
+
+            aTimer.Stop();
+        }
+    }
+
+}
