f4505ca722800654efa9c5c18325a358f2115d18
jairokooistra <44971993+jairokooistra@users.noreply.github.com>
2019-04-15 11:16:20 +0200

    test

---
+++ s1-be-m09/9.5/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading;
+using System.Timers;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Writing_to_a_Text_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[1];
+
+            Console.WriteLine("geef tijd/datum om u clock tezetten");
+
+            for (int i = 0; i < 1; i++)
+            {
+                names[i] = Console.ReadLine();
+            }
+
+            StreamWriter SW = new StreamWriter(@"C:\Users\mylok\source\repos\4\4\bin\Debug\hallo");
+
+            for (int i = 0; i < 1; i++)
+            {
+                SW.WriteLine(names[i]);
+            }
+
+            SW.Close();
+
+            StreamReader();
+
+
+        }
+        static void StreamReader()
+        {
+            string[] names2 = new string[1];
+
+            StreamReader SR = new StreamReader(@"C:\Users\mylok\source\repos\4\4\bin\Debug\hallo");
+
+
+            for (int i = 0; i < 1; i++)
+            {
+                names2[i] = SR.ReadLine();
+            }
+
+            SR.Close();
+
+            Console.WriteLine("Hier is wat je hebt geschreven");
+
+            for (int i = 0; i < 1; i++)
+            {
+                Console.WriteLine(names2[i]);
+            }
+
+            string[] names = names2[1].Split(';');
+
+            while (true)
+            {
+                if (names[1] = DateTime.Today.ToString())
+                {
+                    System.Media.SoundPlayer player = new System.Media.SoundPlayer("C:/Users/mylok/OneDrive/Documents/wolf.wav");
+                    player.Play();
+                    Console.WriteLine("Wolf");
+                    Console.ReadLine();
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
